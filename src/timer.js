857d0956716fc84a685b4f684c947749604d33a6
Kucicowy <kucicowy@mail.ru>
2019-11-22 12:53:14 +0100

    Initial structure and styling

---
+++ src/timer.js
@@ -0,0 +1,16 @@
+function odliczanie() {
+  const dzisiaj = new Date();
+
+  const godzina = dzisiaj.getHours();
+  if (godzina < 10) godzina = "0" + godzina;
+
+  const minuta = dzisiaj.getMinutes();
+  if (minuta < 10) minuta = "0" + minuta;
+
+  const sekunda = dzisiaj.getSeconds();
+  if (sekunda < 10) sekunda = "0" + sekunda;
+
+  document.getElementById("zegar").innerHTML = godzina + ":" + minuta + ":" + sekunda;
+
+  setTimeout("odliczanie()", 1000);
+}
