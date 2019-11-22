2d5ec501925e44c7c549d5e99a89f8c38636b310
Kucicowy <kucicowy@mail.ru>
2019-11-22 15:50:56 +0100

    Update1

--- timer.js
+++ timer.js
@@ -1,14 +1,14 @@
 function odliczanie()
 	{
-		var dzisiaj = new Date();
+		const dzisiaj = new Date();
 		
-		var godzina = dzisiaj.getHours();
+		const godzina = dzisiaj.getHours();
 		if (godzina<10) godzina = "0"+godzina;
 		
-		var minuta = dzisiaj.getMinutes();
+		const minuta = dzisiaj.getMinutes();
 		if (minuta<10) minuta = "0"+minuta;
 		
-		var sekunda = dzisiaj.getSeconds();
+		const sekunda = dzisiaj.getSeconds();
 		if (sekunda<10) sekunda = "0"+sekunda;
 		
 		document.getElementById("zegar").innerHTML = godzina+":"+minuta+":"+sekunda;
