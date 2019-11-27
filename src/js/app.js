6a65c60804abdd8607bf25ee3f17b2a4601a82a0
Lukasz Krzyskow <lukaszkrzyskoww@wp.pl>
2019-11-27 16:27:55 +0100

    call

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,13 @@
 import "../css/main.css";
 import { displaySuggestions } from "./shared/suggestions.js";
 import { getCityFromForm } from "./shared/fomCity.js";
+import { setMap } from "./mapa.js";
 
 const init = () => {
   const input = document.querySelector("#search");
   const btnSearch = document.querySelector("#btn-search");
   btnSearch.addEventListener("click", getCityFromForm);
-  input.addEventListener("keyup", displaySuggestions);
+  //input.addEventListener("keyup", displaySuggestions);
 };
-
+document.getElementById("container").innerHTML=setMap;
 init();
--- src/js/mapa.js
+++ src/js/mapa.js
@@ -1,8 +1,12 @@
 YOUR_API_KEY="AIzaSyA4KXOmdrvv4s_66p4tWFkahTwuaOpu-hY";
+
+export function setMap(){
 fetch("https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap")
-    .then(map => map.json())
+    .then(map => {
+	return map.json()})
 	.then(map => {
 	
-        console.log(map);
+      console.log(map);
 		
-    })
+    });
+}
