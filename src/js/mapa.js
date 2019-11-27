4158123c6321d0b06745f07fe1d0660fa8a46628
Lukasz Krzyskow <lukaszkrzyskoww@wp.pl>
2019-11-27 16:32:27 +0100

    call

--- src/js/mapa.js
+++ src/js/mapa.js
@@ -3,10 +3,10 @@ YOUR_API_KEY="AIzaSyA4KXOmdrvv4s_66p4tWFkahTwuaOpu-hY";
 export function setMap(){
 fetch("https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap")
     .then(map => {
-	return map.json()})
-	.then(map => {
+	return map.json()});
+	//.then(map => {
 	
-      console.log(map);
+    //  console.log(map);
 		
-    });
+   // });
 }
