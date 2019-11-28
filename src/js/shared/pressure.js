53dbc1a019ce7f2840e7cbaeebc3fc255394c3d8
Karol Wolski <karolwolski04@gmail.com>
2019-11-28 18:14:16 +0100

    Revert "Precipitation (#11)"
    This reverts commit 00ff2e89a683a561508904ab92c1fed1805acde9.

--- src/js/shared/pressure.js
+++ src/js/shared/pressure.js
@@ -5,7 +5,7 @@ export const getPressure = (param) => {
         const htmlPressure = document.getElementById("airpressure");
         htmlPressure.innerHTML = "";
         let i;
-        for (i = 0; i < 5; i++) {
+        for (i = 0; i < 6; i++) {
             const pressure = data.list[i].main.pressure;
             const pressureTime = (data.list[i].dt_txt).substring(11, 16);
             htmlPressure.innerHTML += `<div><span>${pressure}hPa</span> <span>${pressureTime}</span></div>`;
