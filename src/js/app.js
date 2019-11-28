ed0f048fcce6198e5c5cd3a0ed007a21ee0f5a67
Mateusz <skaua@o2.pl>
2019-11-28 14:54:57 +0100

    Revert "All"
    This reverts commit 9d42ab650e851803632948f5a187298356b7e5a1.

--- src/js/app.js
+++ src/js/app.js
@@ -1,18 +1,12 @@
 import "../css/main.css";
 import { displaySuggestions } from "./shared/suggestions.js";
 import { getCityFromForm } from "./shared/fomCity.js";
-import { geolocation } from './shared/geolocation.js';
-import {getPressure} from './shared/pressure.js';
 
 const init = () => {
   const input = document.querySelector("#search");
   const btnSearch = document.querySelector("#btn-search");
-  const useCurrentLocation = document.querySelector("#useCurrentLocation")
   btnSearch.addEventListener("click", getCityFromForm);
-  input.addEventListener("keyup", displaySuggestions);
-  useCurrentLocation.addEventListener("click", geolocation);
-
-  geolocation();
+  // input.addEventListener("keyup", displaySuggestions);
 };
-getPressure('Szczecin');
+
 init();
