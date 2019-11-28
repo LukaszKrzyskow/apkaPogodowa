02e33b52a0a673b6e5e6063a180c6389fad6161f
Mateusz <skaua@o2.pl>
2019-11-28 16:01:34 +0100

    All IN ;)

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,17 @@
 import "../css/main.css";
 import { displaySuggestions } from "./shared/suggestions.js";
 import { getCityFromForm } from "./shared/fomCity.js";
+import { geolocation } from './shared/geolocation.js';
 
 const init = () => {
   const input = document.querySelector("#search");
   const btnSearch = document.querySelector("#btn-search");
+  const useCurrentLocation = document.querySelector("#useCurrentLocation")
   btnSearch.addEventListener("click", getCityFromForm);
   // input.addEventListener("keyup", displaySuggestions);
+  useCurrentLocation.addEventListener("click", geolocation);
+
+  geolocation();
 };
 
 init();
