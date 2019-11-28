9b0d84bc6f911baff547b35b420c7b29aa030ed8
Kucicowy <kucicowy@mail.ru>
2019-11-28 17:18:00 +0100

    Edited precipitation tile

--- src/js/shared/fomCity.js
+++ src/js/shared/fomCity.js
@@ -2,8 +2,12 @@ import { addToLocalStorage } from "../localStorage/localStorage.js";
 import { getPressure } from "./pressure.js";
 import { getHumidity } from "./humidity.js";
 import { getForecast } from "./getForecast.js";
+<<<<<<< HEAD
 import { getGeneral } from "./general.js";
 import { getForecastData } from "./getForecastData.js";
+=======
+import { getPrecipitation } from "./precipitation.js";
+>>>>>>> Edited precipitation tile
 export const getCityFromForm = e => {
   e.preventDefault();
   const input = document.querySelector("#search");
@@ -14,6 +18,10 @@ export const showData = param => {
   getPressure(param);
   getHumidity(param);
   getForecast(param);
+<<<<<<< HEAD
   getGeneral(param);
   getForecastData(param);
+=======
+  getPrecipitation(param);
+>>>>>>> Edited precipitation tile
 };
